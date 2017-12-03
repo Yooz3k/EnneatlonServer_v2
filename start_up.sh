8d6ade3aea125d0f0e64f405e1db15313594f21a
TymekKonkol <tymek.konkol@hotmail.com>
2017-12-03 20:39:49 +0100

    Added scipts

---
+++ start_up.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#pobranie z gita strony i bazy
+git clone https://github.com/TymekKonkol/Enneatlon_v2.git website
+docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker rmi beenneatlon_presta
+docker rmi beenneatlon_mydatabase
+docker-compose build
+docker-compose up -d
+cat website/backup.sql | docker exec -i mydatabase /usr/bin/mysql -u root --password=1234 mysql
